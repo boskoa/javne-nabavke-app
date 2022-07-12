c7336d84ab156b550845c92710f254e75809ed46
boskoa <bosko.ap@gmail.com>
2022-07-12 23:18:59 +0200

    fixing sequelize dialect

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,7 +2,9 @@ const { Sequelize } = require('sequelize')
 const { DATABASE_URL } = require('./config')
 const { SequelizeStorage, Umzug } = require('umzug')
 
-const sequelize = new Sequelize(DATABASE_URL)
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialect: 'postgres'
+})
 
 const connectToDatabase = async () => {
   try {
